73a69b25c2b6b0a051f83021431731c2d2f69640
nguyenminhthoa1002 <minhthoa100299@gmail.com>
2022-11-03 15:44:26 +0700

    first-commit

---
+++ Bai1-CoSoDuLieuQuanHe.sql
@@ -0,0 +1,9 @@
+create database md3_bai1_cosodulieuquanhe;
+-- drop database md3_bai1_cosodulieuquanhe;
+use md3_bai1_cosodulieuquanhe;
+create table new_table(
+id int auto_increment,
+productname nvarchar(15),
+primary key(id)
+);
+drop table new_table;
